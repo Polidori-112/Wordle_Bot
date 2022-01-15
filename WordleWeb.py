1d780805087c924184c9db329827003a52651bbc
Lucas Polidori <92409632+Polidori-112@users.noreply.github.com>
2022-01-15 08:33:19 -0800

    Update WordleWeb.py
    fixed bug to improve percentage of correct guesses to 96.8%

--- WordleWeb.py
+++ WordleWeb.py
@@ -1,3 +1,10 @@
+import time
+import selenium
+import re
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+            
 #create dictionary of words
 dict = {}				
 file = open("words.txt", "r")
@@ -11,19 +18,6 @@ for word in file:
 del dict['']   
 
 
-#simulate the wordle game
-def wordle(guess, letters, answ, final, wrong_guess):
-    for c in range (0,5):
-        if (answ[c] == guess[c]):
-            letters = letters + guess[c]
-            final[c] = guess[c]
-        
-        if (answ[c] != guess[c] and (guess[c] == answ[0] or guess[c] == answ[1]
-        or guess[c] == answ[2] or guess[c] == answ[3] or guess[c] == answ[4])):
-            letters = letters + guess[c]
-            wrong_guess[c] = guess[c]
-            
-    return letters
 #remove double letters from a guess
 def clean(let):    
     result = "".join(dict.fromkeys(let))
@@ -43,10 +37,11 @@ def jumble(word, dict):
 def remove(guesses, wrong_guess, final):
     for n in range(0, len(guesses)):
         for i in range (0, 5):
-            if ((guesses[n][i] != final[i] or guesses[n][i] == wrong_guess[i])
-            and final[i] != ''):
-                del guesses[n]
-                return 
+            for j in range(0, len(wrong_guess[i])):
+                if ((guesses[n][i] != final[i] and final[i] != '') 
+                or guesses[n][i] == wrong_guess[i][j]):
+                    del guesses[n]
+                    return 
 #add new possible answers to list of possible answers            
 def append_answers(guesses, wrong_guess, final, answers):
     #remove incorrect answers               
@@ -80,43 +75,92 @@ def check_answers(let, wrong_guess, final, answers):
 #main solving operation
 #guess 5 set words to remove almost all possible letters
 #unscramble the known letters to get possible guess
-def method(answ):
+def method():
     let = ''
     #5 words that are guessed before program creates final guess
     words = ['quick', 'brown', 'shady', 'cleft', 'gimps']
     final = ['', '', '', '', '']
-    wrong_guess = ['', '', '', '', '']
+    wrong_guess = [[''], [''], [''], [''], ['']]
     answers = []
     #test the words in the array
     for i in range(0, 5):
         test = words[i]
-        let = wordle(test, let, answ, final, wrong_guess)
+        let = enter_word(test, let, final, wrong_guess)
         let = clean(let)
     
     check_answers(let, wrong_guess, final, answers)
-    #print feedback on the game
-    print(answ)        
+    #print feedback on the game        
     print(answers)
-    #used for testing to determine correct percentage
-    if (len(answers) == 1 and answers[0] == answ):
-        return 1
-    else:
-        return 0
-#test for percentage of wins vs losses in every possible answer    
-def test():
-    correct = 0
-    total = 0
     
-    for word in dict:
-        if (method(word) == 1):
-            correct = correct + 1
-            total = total + 1
-        else:
-            total = total + 1
-        # 
-    print('\nNumber Correct: ' + str(correct))
-    print('Total: ' + str(total))
-    print('Percentage: ' + str(correct / total))
+    return answers[0]
 
+#open wordle website and start playing game
+browser = webdriver.Firefox()
+browser.get('https://www.powerlanguage.co.uk/wordle/')
 
-test()
+time.sleep(1)
+
+Elem = browser.find_element_by_tag_name('html')
+
+Elem.click()
+
+time.sleep(1)
+
+#enters guess into wordle and returns known letters/changes final and wrong_guess arrays
+def enter_word(word, let, final, wrong_guess):
+    Elem.send_keys(word)
+    Elem.send_keys(Keys.ENTER)
+    
+    time.sleep(1)
+    
+    
+    host = browser.find_element_by_tag_name("game-app")
+    firstHost = browser.find_element_by_tag_name("game-app")
+    game = browser.execute_script("return arguments[0].shadowRoot.getElementById('game')", host)
+    
+    
+    keyboard = game.find_element_by_tag_name("game-keyboard")
+    
+    keys = browser.execute_script("return arguments[0].shadowRoot.getElementById('keyboard')", keyboard)
+    
+    time.sleep(2)
+    #store data of all the letters (match, match in different location, no match)
+    keydata = browser.execute_script("return arguments[0].innerHTML;",keys)
+    #get array of matches and matches in different locations using janky regex logic
+    correctRegex = re.compile('...............correct', re.VERBOSE)
+    
+    matches = ['', '', '', '', '']
+    n = 0
+    for groups in correctRegex.findall(keydata):
+        matches[n] = (groups[0])
+        n = n + 1
+        
+    presentRegex = re.compile('...............present', re.VERBOSE)
+    
+    nearmatches = ['', '', '', '', '']
+    n = 0
+    for groups in presentRegex.findall(keydata):
+        nearmatches[n] = (groups[0])
+        n = n + 1
+        
+    #add known values to arrays used in method
+    for i in range (0,5):
+        for j in range(0, 5):
+            if (word[i] == matches[j]):
+                final[i] = word[i]
+                let = ''.join((let, final[i]))
+            if (word[i] == nearmatches[j]):
+                wrong_guess[i] = word[i]
+                let = ''.join((let, word[i]))
+    return let
+
+#give time for user to cancel and add suspense
+time.sleep(1)
+#input final guess
+Elem.send_keys(method())
+Elem.send_keys(Keys.ENTER)
+
+#allow time to celebrate solving it
+time.sleep(8)        
+
+browser.close()
